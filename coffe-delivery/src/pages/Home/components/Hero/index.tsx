2efe83acb05b44b4fb30f1a59a129335e0c9ab74
josmar-jr <josmartrigueiro.jr@gmail.com>
2022-09-19 19:10:44 -0300

    Initial commit

---
+++ coffe-delivery/src/pages/Home/components/Hero/index.tsx
@@ -0,0 +1,11 @@
+import { HeroContainer, HeroContent } from "./styles";
+
+export function Hero() {
+  return (
+    <HeroContainer>
+      <HeroContent>
+        
+      </HeroContent>
+    </HeroContainer>
+  );
+}
